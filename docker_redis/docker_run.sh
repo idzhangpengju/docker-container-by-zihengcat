5f7456001f82338a30a261db0b4a4c41868d3555
idzhangpengju <57204556+idzhangpengju@users.noreply.github.com>
2019-11-08 11:10:48 +0800

    Update docker_run.sh

--- docker_redis/docker_run.sh
+++ docker_redis/docker_run.sh
@@ -19,8 +19,5 @@ sudo docker run -d -i -t \
                 --env LC_ALL=en_US.UTF-8 \
                 --name redis \
                 -p 0.0.0.0:6379:6379 \
-                ziheng/centos:redis \
-                ${REDIS_OPTIONS}
-#/usr/sbin/init
-# ------------------------------
-# EOF
+                zhangpengju/centos:redis \
+                redis-server ${REDIS_OPTIONS}
